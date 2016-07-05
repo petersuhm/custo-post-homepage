2dc6ee940272e0061d5cf46f7bc26e605f808bc7
Peter Suhm <peter@suhm.dk>
2016-07-05 21:29:16 +0200

    Update custom-post-homepage.php

--- custom-post-homepage.php
+++ custom-post-homepage.php
@@ -4,18 +4,17 @@
  * Plugin Name: Custom Post Homepage
  */
 
-add_filter( 'get_pages', function( $pages, $r ) {
-    if ( ! isset( $r[ 'name' ] ) )
-        return $pages;
-
-    if ( 'page_on_front' == $r[ 'name' ] ) {
-        $args = array(
-            'post_type' => 'aops_landing_page'
-        );
-
-        $portfolios = get_posts( $args );
-        $pages = array_merge( $pages, $portfolios );
-    }
+add_filter( 'get_pages', function( $pages ){
+    $args = array(
+        'post_type' => 'aops_landing_page'
+    );
+    $items = get_posts($args);
+    $pages = array_merge($pages, $items);
 
     return $pages;
-}, 10, 2 );
+} );
+
+add_action( 'pre_get_posts', function( $query ){
+    if('' == $query->query_vars['post_type'] && 0 != $query->query_vars['page_id'])
+        $query->query_vars['post_type'] = array( 'page', 'aops_landing_page' );
+} );
